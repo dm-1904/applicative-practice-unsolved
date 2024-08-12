3071390a8267a79adc48ed6eb08d78799556a93e
damonryon <damon.ryon@gmail.com>
2024-08-12 14:22:31 -0700

    solve e5

--- exercises/e5.js
+++ exercises/e5.js
@@ -5,7 +5,8 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsWithMassValue(data, number) {
-  // Your code goes here...
+  return data.planets.filter((planet) => planet.mass.massValue >= number)
+    .map((name) => name.name)
 }
 
 
